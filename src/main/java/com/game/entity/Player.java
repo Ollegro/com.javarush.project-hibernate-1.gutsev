babee27d26a22d5c7212f20b4fd3edb6736c3ebe
Oleg Gutsev <o.gutsev@mail.ru>
2024-07-15 19:27:10 +0300

    upload

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,28 +1,43 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
 import java.util.Date;
 
-
+@Entity
+@Table (schema = "rpg",name = "player")
+@NamedQuery(name = "Player.getAllCount", query = "SELECT COUNT(p) FROM Player p")
 public class Player {
+
+    @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id",nullable = false)
     private Long id;
 
+    @Column (length = 12, nullable = false)
     private String name;
 
+    @Column(length = 30, nullable = false)
     private String title;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column (nullable = false)
     private Race race;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column (nullable = false)
     private Profession profession;
 
+    @Column (nullable = false)
     private Date birthday;
 
+    @Column (nullable = false)
     private Boolean banned;
 
+    @Column (nullable = false)
     private Integer level;
 
     public Player() {
     }
-
     public Player(Long id, String name, String title, Race race, Profession profession, Date birthday, Boolean banned, Integer level) {
         this.id = id;
         this.name = name;
@@ -37,63 +52,48 @@ public class Player {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public Race getRace() {
         return race;
     }
-
     public void setRace(Race race) {
         this.race = race;
     }
-
     public Profession getProfession() {
         return profession;
     }
-
     public void setProfession(Profession profession) {
         this.profession = profession;
     }
-
     public Date getBirthday() {
         return birthday;
     }
-
     public void setBirthday(Date birthday) {
         this.birthday = birthday;
     }
-
     public Boolean getBanned() {
         return banned;
     }
-
     public void setBanned(Boolean banned) {
         this.banned = banned;
     }
-
     public Integer getLevel() {
         return level;
     }
-
     public void setLevel(Integer level) {
         this.level = level;
     }
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,27 +1,62 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import jakarta.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Player.class)
+                .setProperties(properties)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        List<Player> players;
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            String sql = "SELECT * FROM rpg.player";
+            players = session.createNativeQuery(sql, Player.class)
+                    .setFirstResult(pageNumber  * pageSize)
+                    .setMaxResults(pageSize)
+                    .getResultList();
+
+        }
+
+
+        return players;
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+        int count = 0;
+
+
+        return count;
     }
 
     @Override
@@ -46,6 +81,6 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
